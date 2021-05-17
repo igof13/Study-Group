a152ea79252b509d4f5c50d7341b045c5bc3f333
igor fugagnoli <igof13@gmail.com>
2021-05-17 16:46:51 -0300

    Adding Javascript

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-
-
-$(document).ready(function(){
-    
-  });
+function showTheme(theme){
+  
+const themes = document.getElementsByClassName('my-cards');
+for (var i = 0; i < themes.length; i++)
+{
+  if(theme == themes[i].id)
+  {
+    themes[i].style = "display: block";
+  }
+  else
+  themes[i].style = "display: none";
+}
+}
